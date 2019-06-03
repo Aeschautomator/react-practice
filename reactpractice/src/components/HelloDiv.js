18056c22832e6f64c4cb76f82338a78b3ecd253c
Alex esch <eschmassage@gmail.com>
2019-06-03 16:56:38 -0400

    helloDiv practice

---
+++ reactpractice/src/components/HelloDiv.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloDiv() {
+  return (
+    <div>
+      <h1>Hello! My name is Alex Esch</h1>`.
+      <h2>Things I like!</h2>
+      <ul>
+        <li>Food</li>
+        <li>Video Games</li>
+        <li>Love</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
